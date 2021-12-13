b394117bb876ebea283f481a148746d49f8a22b8
gayathrikaranam <90285694+gayathrikaranam@users.noreply.github.com>
2021-12-13 02:53:11 -0500

    Add files via upload

--- calculator/main.py
+++ calculator/main.py
@@ -1,25 +1,87 @@
-""" This is the increment function"""
+""" import all the methods from calc_methods"""
+import logging
+import sys
+from calculator.calculator_calculations.addition import Addition
+from calculator.calculator_calculations.subtraction import Subtraction
+from calculator.calculator_calculations.multiplication import Multiplication
+from calculator.calculator_calculations.division import Division
+from calculator.history_calculations.history_calculations import History
+from csv_handling.read_csv import CSVRead
+
+sys.tracebacklimit = 0
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('log/sample.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
 class Calculator:
-    """ This is the Calculator class"""
+    """ Creating a Module Calculator """
+    # result set to 0 for initialization
+    history = []
+    data = []
+    path = ''
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
+    def __init__(self, path):
+        self.data = CSVRead.read_data(path)
+        self.path = path
+        self.move_path = path.replace("input", "done")
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+    def get_data(self):
+        """ Splits the data and returns all columns """
+        csv_data = self.data
+        num1 = csv_data['num_1'].values
+        num2 = csv_data['num_2'].values
+        add = [round(i, 3) for i in csv_data['add_nums'].values]
+        sub = [round(i, 3) for i in csv_data['sub_nums'].values]
+        multi = [round(i, 3) for i in csv_data['mult_nums'].values]
+        div = [round(i, 3) for i in csv_data['div_nums'].values]
+
+        return num1, num2, add, sub, multi, div
+
     @staticmethod
-    def divide_numbers(value_a, value_b):
-        """ divide two numbers and store the result"""
-        return value_a / value_b
+    def add_nums(*args):
+        """ Adds given list of numbers and appends the result to history """
+        addition = Addition(args).getresult()
+        logger.debug('Add: %f + %f = %f', args[0], args[1], addition)
+        History.add_calculation_to_history(addition)
+        return History.get_last_calculation_added()
+
+    @staticmethod
+    def subtract_nums(*args):
+        """ Subtracts given list of numbers and appends the result to history """
+        subtraction = Subtraction(args).getresult()
+        logger.debug('Subtract: %f - %f = %f', args[0], args[1], subtraction)
+        History.add_calculation_to_history(subtraction)
+        return History.get_last_calculation_added()
+
+    @staticmethod
+    def multiply_nums(*args):
+        """ Multiplies given list of numbers and appends the result to history """
+        multiplication = Multiplication(args).getresult()
+        logger.debug('Multiply: %f * %f = %f', args[0], args[1], multiplication)
+        History.add_calculation_to_history(multiplication)
+        return History.get_last_calculation_added()
+
+    @staticmethod
+    def divide_nums(*args):
+        """ Divides given list of numbers and appends the result to history """
+        try:
+            division = Division(args).getresult()
+            History.add_calculation_to_history(division)
+        except ZeroDivisionError as err:
+            logger.exception(err)
+            return 0.0
+        else:
+            logger.debug('Divide: %f / %f = %f', args[0], args[1], division)
+            return History.get_last_calculation_added()
